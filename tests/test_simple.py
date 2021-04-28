58aa59a7e586f447938bc582bfde3e66d9c26e22
Genki Matsunaga <genki.matsunaga@gmail.com>
2021-04-29 05:42:36 +0900

    Change test

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -2,7 +2,7 @@ from calcpi import simple
 
 
 class TestSimplePi:
-    def test_pi_one(self):
-        actual = simple.pi()
+    def test_pi_round2(self):
+        actual = simple.pi(2)
         expected = 3.14
-        assert actual >= expected
+        assert actual == expected
