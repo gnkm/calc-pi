45fb2602f26394d66904d926f7bd7fefbaf03768
Genki Matsunaga <genki.matsunaga@gmail.com>
2021-05-02 22:51:03 +0900

    Add a test

--- tests/test_value.py
+++ tests/test_value.py
@@ -9,3 +9,11 @@ class TestValuePi:
             mpmath.mpf('3.14'), 3
         )
         assert actual == expected
+
+    def test_pi_accuracy21(self):
+        accuracy = 21
+        actual = mpmath.nstr(value.pi(accuracy), accuracy)
+        expected = mpmath.nstr(
+            mpmath.mpf('3.14159265358979323846'), accuracy
+        )
+        assert actual == expected
