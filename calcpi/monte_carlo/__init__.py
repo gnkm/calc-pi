5b928e85a87826084b34337bd17d895d7253ad5d
Genki Matsunaga <genki.matsunaga@gmail.com>
2021-06-18 22:01:35 +0900

    Add docstring

--- calcpi/monte_carlo/__init__.py
+++ calcpi/monte_carlo/__init__.py
@@ -1,19 +1,23 @@
 """Get pi with Monte Carlo simulation."""
 
-from mpmath import (
-    mp,
-    mpf,
-)
+import mpmath
 import numpy as np
 
 from calcpi import print_prettify  # noqa: F401  # pylint: disable=unused-import
 
 
-def pi(accuracy: int) -> float:  # pylint: disable=invalid-name
-    mp.dps = accuracy
+def pi(accuracy: int) -> mpmath.mpf:  # pylint: disable=invalid-name
+    """Return Pi gotten by Monte Carlo simulation.
+
+    Args:
+        accuracy (int): [description]
+
+    Returns:
+        mpmath.mpf: Pi
+    """
+    mpmath.mp.dps = accuracy
     points = np.random.uniform(size=(accuracy, 2))
     point_count = points.shape[0]
     distances = np.power(points[:, 0], 2) + np.power(points[:, 1], 2)
     inner_circle_point_count = np.count_nonzero(distances <= 1)
-
-    return mpf(4) * inner_circle_point_count / point_count
+    return mpmath.mpf(4) * inner_circle_point_count / point_count
