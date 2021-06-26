dfcdb5cf8551a11fb161c9264b345d3506321844
Genki Matsunaga <genki.matsunaga@gmail.com>
2021-06-26 18:17:31 +0900

    Add subcommand calc package

--- calcpi/__main__.py
+++ calcpi/__main__.py
@@ -11,6 +11,8 @@ from calcpi.algorithms import gauss_legendre  # noqa: F401  # pylint: disable=un
 from calcpi.algorithms import monte_carlo  # noqa: F401  # pylint: disable=unused-import
 from calcpi.algorithms import regular_polygon as polygon  # noqa: F401  # pylint: disable=unused-import
 
+from calcpi.subcommands import calc as sc_calc
+
 from calcpi import print_prettify  # noqa: F401  # pylint: disable=unused-import
 from calcpi import utils
 
@@ -28,15 +30,20 @@ ERROR_ACCRACY: int = 2
 
 def main():
     # pylint: disable=missing-function-docstring
-    exec_subcommand()
+    parser = argparse.ArgumentParser(
+        description='Calcurates Pi',
+        prog='python -m calcpi',
+    )
+    subparsers = parser.add_subparsers(prog='python -m calcpi')
+    subparsers = sc_calc.define_args(subparsers)
+
+    args = parser.parse_args()
+    if hasattr(args, 'handler'):
+        args.handler(args)
+    else:
+        parser.print_help()
+
     sys.exit()
-
-
-def subcommand_calc(args: argparse.Namespace) -> None:
-    # pylint: disable=missing-function-docstring
-    pi: mpmath.mpf = calc(args.algorithm,  args.accuracy)  # pylint: disable=invalid-name
-    formated_pi: str = utils.format_pi(pi, args.accuracy, args.separated)
-    sys.stdout.write(formated_pi)
 
 
 def subcommand_evaluate(args: argparse.Namespace) -> None:
@@ -44,20 +51,6 @@ def subcommand_evaluate(args: argparse.Namespace) -> None:
     err: mpmath.mpf = evaluate(args.algorithm, args.accuracy)
     formated_err: str = utils.format_pi(err, args.accuracy)
     sys.stdout.write(formated_err)
-
-
-def calc(algorithm: str, accuracy: int) -> mpmath.mpf:
-    """Return Pi.
-
-    Args:
-        accuracy (int): accuracy
-        algorithm (str): algorithm by which pi is calcurated
-
-    Returns:
-        mpmath.mpf: Pi value
-    """
-    pi: mpmath.mpf = globals()[algorithm].pi(accuracy)  # pylint: disable=invalid-name
-    return pi
 
 
 def evaluate(algorithm: str, accuracy: int) -> mpmath.mpf:
@@ -84,38 +77,6 @@ def exec_subcommand() -> None:
     subparsers = parser.add_subparsers(
         prog='python -m calcpi',
     )
-
-    # ===== calc subcommand =====
-    parser_calc = subparsers.add_parser('calc')
-    parser_calc.add_argument(
-        'algorithm',
-        help='Algorithm by which pi is calcurated',
-        choices=ALGORITHMS,
-    )
-    parser_calc.add_argument(
-        '--accuracy',
-        default=10,
-        type=int,
-        help='accuracy',
-    )
-    parser_calc.add_argument(
-        '-l',
-        '--list',
-        help='show algorithms list',
-    )
-    parser_calc.add_argument(
-        '-s',
-        '--separated',
-        action='store_true',
-        help='show separated number',
-    )
-    parser_calc.add_argument(
-        '--grouped_digit',
-        default=10,
-        type=int,
-        help='number of digits to be summarized when displaying',
-    )
-    parser_calc.set_defaults(handler=subcommand_calc)
 
     # ===== evaluate subcommand =====
     parser_evaluate = subparsers.add_parser('evaluate')
---
+++ calcpi/subcommands/calc.py
@@ -0,0 +1,81 @@
+"""Define args of calc subcommand.
+"""
+
+
+import argparse
+import sys
+from typing import List
+
+import mpmath
+
+from calcpi.algorithms import actual  # noqa: F401  # pylint: disable=unused-import
+from calcpi.algorithms import gauss_legendre  # noqa: F401  # pylint: disable=unused-import
+from calcpi.algorithms import monte_carlo  # noqa: F401  # pylint: disable=unused-import
+from calcpi.algorithms import regular_polygon as polygon  # noqa: F401  # pylint: disable=unused-import
+
+from calcpi import print_prettify  # noqa: F401  # pylint: disable=unused-import
+from calcpi import utils
+
+
+SUBCOMMAND_NAME: str = 'calc'
+
+ALGORITHMS: List[str] = [
+    'actual',
+    'gauss_legendre',
+    'monte_carlo',
+    'polygon',
+]
+
+
+def define_args(subparsers):
+    parser = subparsers.add_parser(SUBCOMMAND_NAME)
+    parser.add_argument(
+        'algorithm',
+        help='Algorithm by which pi is calcurated',
+        choices=ALGORITHMS,
+    )
+    parser.add_argument(
+        '--accuracy',
+        default=10,
+        type=int,
+        help='accuracy',
+    )
+    parser.add_argument(
+        '-l',
+        '--list',
+        help='show algorithms list',
+    )
+    parser.add_argument(
+        '-s',
+        '--separated',
+        action='store_true',
+        help='show separated number',
+    )
+    parser.add_argument(
+        '--grouped_digit',
+        default=10,
+        type=int,
+        help='number of digits to be summarized when displaying',
+    )
+    parser.set_defaults(handler=subcommand)
+
+
+def calc(algorithm: str, accuracy: int) -> mpmath.mpf:
+    """Return Pi.
+
+    Args:
+        accuracy (int): accuracy
+        algorithm (str): algorithm by which pi is calcurated
+
+    Returns:
+        mpmath.mpf: Pi value
+    """
+    pi: mpmath.mpf = globals()[algorithm].pi(accuracy)  # pylint: disable=invalid-name
+    return pi
+
+
+def subcommand(args: argparse.Namespace) -> None:
+    # pylint: disable=missing-function-docstring
+    pi: mpmath.mpf = calc(args.algorithm,  args.accuracy)  # pylint: disable=invalid-name
+    formated_pi: str = utils.format_pi(pi, args.accuracy, args.separated)
+    sys.stdout.write(formated_pi)
